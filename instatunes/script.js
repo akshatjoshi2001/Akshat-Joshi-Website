e1e931fc3e8200e11ecde04c038acafb9983c1ba
Akshat Joshi <akshatjoshi375@gmail.com>
2020-05-05 22:02:13 +0530

    UI Changes

--- instatunes/script.js
+++ instatunes/script.js
@@ -2,9 +2,9 @@ function play()
 {
     var query = document.getElementById("q");
     q = query.value;
-    document.getElementById("audio").innerHTML="Please Wait. Getting your song :)...."
+    document.getElementById("audio").innerHTML="<br /><b><span style='color:blue;font-size:20px;'>Getting your song.  :)</span><br /><img src='loading.gif' height='200px' width='300px' alt='Loading...' /> <br /> "
     axios.get("http://instatunes.000webhostapp.com/music.php?q="+q).then((resp)=>{
-        document.getElementById("audio").innerHTML="Here it is. Enjoy!<br />" + resp.data;
+        document.getElementById("audio").innerHTML="<br /><br />Here it is. <b style='color:blue;'>Enjoy!</b><br /><br />" + resp.data;
     });
 
 }
